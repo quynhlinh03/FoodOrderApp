8b6f57628b5c96965eb28c14d5ea9bb38fff8074
quynhlinh03 <phamthiquynhlinh3112002@gmail.com>
2023-08-01 22:53:14 +0700

    Submitting order to a BE server

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -56,6 +56,10 @@ const cartReducer = (state, action) => {
     };
   }
 
+  if (action.type === 'CLEAR') {
+    return defaultCartState
+  }
+
   return defaultCartState;
 };
 
@@ -70,11 +74,15 @@ const CartProvider = (props) => {
     dispatchCartAction({ type: 'REMOVE', id: id });
   };
 
+  const clearCarHandler = () => {
+    dispatchCartAction({ type: 'CLEAR' })
+  }
   const cartContext = {
     items: cartState.items,
     totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
+    clearCart: clearCarHandler
   };
 
   return (
